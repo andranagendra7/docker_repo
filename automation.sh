4625d59da8f9be6352b5e074e5334b2bd401dd58
andranagendra720@gmail.com <andranagendra720@gmail.com>
2019-05-09 11:43:52 +0000

    add uploding the warfie to docker repository

---
+++ automation.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+git init
+
+git config --global user.name "andranagendra7"
+
+git config --global user.name "andranagendra720@gmail.com"
+
+
+git add /var/lib/jenkins/workspace/maven_job/target/*.war
+
+cd /var/lib/jenkins/workspace/maven_job/
+
+git add dockerfile 
+
+git add java.sh
+
+git add tomcat.sh
+
+git add context.xml 
+
+git add tomcat-users.xml
+
+git add .
+
+git remote add origin_ssh git@github.com:andranagendra7/docker_repo.git
+
+git commit -m "add uploding the warfie to docker repository"
+
+git push origin_ssh master
+
+
